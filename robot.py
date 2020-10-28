83372a9784e052ae4b2de47d26eb1a2f7758df46
Amy Grace <54113654+00p513-dev@users.noreply.github.com>
2020-10-28 21:36:14 +0000

    Create robot.py

---
+++ robot.py
@@ -0,0 +1,32 @@
+# Thanks to github.com/usernein
+
+import os
+import pyrogram
+
+with open('Aonly.txt') as f:
+    os.environ['CAT_FILE'] = f.read()
+
+rom = os.getenv('ROM_NAME')
+zip = os.getenv('ZIP_NAME')
+romurl = os.getenv('ROM_URL')
+da = os.getenv('DOWNLOAD_A')
+dab = os.getenv('DOWNLOAD_AB')
+tgname = os.getenv('TG_NAME')
+
+with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('BOT_TOKEN')) as client:
+    client.send_message(
+        text=f"""<b>{tgname} GSI For A/AB Devices</b>
+<b>Firmware Base:</b> <a href="{romurl}">HERE</a>
+<b>Information:</b>
+<code>{cat}</code>
+<b>Notes:</b>
+<i>{notes}</i>
+<b>Download A-Only:</b> <a href="{da}">HERE</a>
+<b>Download A/B:</b> <a href="{dab}">HERE</a>
+<b>Notes: </b>{notes}
+<b>@amyGSI</b> - <i>Channel</i>: @amyGSI
+<a href="https://github.com/amyGSI/ErfanGSIs">Ported using ErfanGSIs fork</a>""",
+        chat_id=os.getenv('CHAT_ID'),
+        parse_mode="html",
+        disable_web_page_preview=True
+    )
